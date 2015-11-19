e619fc3ef0ffe161a30a88a2b6afa1d57e307f82
Oleksander Kostenko <o.kostenko@samsung.com>
2015-11-19 19:28:31 +0200

    TizenRefApp-4912 Track changing of contact's name changing
    Change-Id: Ieb89bacd50be7a538eae0ecb6104bc12ee8ef764
    Signed-off-by: Oleksander Kostenko <o.kostenko@samsung.com>

--- src/Common/ContactManager/inc/ContactManager.h
+++ src/Common/ContactManager/inc/ContactManager.h
@@ -28,21 +28,65 @@
 
 namespace Msg
 {
+    class IContactDbChangeListener;
+
     class ContactManager
     {
         public:
             ContactManager();
             ~ContactManager();
+            ContactManager(ContactManager&) = delete;
+            ContactManager &operator=(ContactManager&) = delete;
 
         public:
+            /*
+             *@brief        function return what kind of error occurred
+             *@param[in]    error - number of error
+             *@return       kind of error
+             */
             static std::string whatError(int error);
-
+            /*
+             *@brief        function search data of contacts person
+             *@param[in]    keyword - keyword which to search data
+             *@return       ContactList
+             */
             template<typename T>
             ContactList<T> search(const std::string &keyword);
+            /*
+             *@brief        function find by number: id, name and thumbnail path
+             *@param[in]    number - contact person number
+             *@return       ContactPersonNumber
+             */
             ContactPersonNumber getContactPersonNumber(const std::string &number) const;
+            /*
+             *@brief        function add listener on contacts database
+             *@param[in]    listener
+             */
+            void addListener(IContactDbChangeListener &listener);
+            /*
+             *@brief        function remove listener
+             *@param[in]    listener - listener to be remove
+             */
+            void removeListener(IContactDbChangeListener &listener);
+
+        private:
+            /*
+             *@brief        The callback function to register when there have been changes in contacts database
+             *@param[in]    view_uri - the view URI
+             *@param[in]    user_data - the user data passed from the callback registration function
+             */
+            static void contactChangedCb(const char *view_uri, void *user_data);
+
+        private:
+            std::list<IContactDbChangeListener *> m_Listeners;
+    };
+
+    class IContactDbChangeListener
+    {
+        public:
+            virtual ~IContactDbChangeListener() {}
+            virtual void onContactChanged() {};
     };
 }
-
-
 
 #endif /* __ContactManager_h__ */
