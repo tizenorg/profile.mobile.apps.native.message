77531fbf43ba8362e5ed94079af8dbec8d2700d1
Denis Dolzhenko <d.dolzhenko@samsung.com>
2015-12-04 15:16:49 +0200

    TizenRefApp-5192 Implement FileUtills class(base function)
    Change-Id: I50567e51e7e538a695b46cb1bd59eaa0680281f7
    Signed-off-by: Denis Dolzhenko <d.dolzhenko@samsung.com>

---
+++ src/Common/Utils/inc/FileUtils.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2009-2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __MSG_FILE_UTIL_H__
+#define __MSG_FILE_UTIL_H__
+
+#include <string>
+
+namespace Msg
+{
+    class FileUtils
+    {
+        public:
+            /**
+             * @brief Get file size.
+             * @param[in] path to file
+             * @retval file size
+             */
+            static long long getFileSize(const std::string &file);
+
+            /**
+             * @brief Check file existing.
+             * @param[in] path to file
+             * @retval Returns true if file exist, else false
+             */
+            static bool isExists(const std::string &file);
+
+            /**
+             * @brief Copy file.
+             * @param[in] src Source
+             * @param[in] dst dst
+             * @retval Returns true if file exist, else false
+             */
+            static bool copy(const std::string &src, const std::string &dst);
+
+            /**
+             * @brief Make Directory.
+             * @param[in] path to directory
+             * @retval Returns true if directory created, else false
+             */
+            static bool makeDir(const std::string &dir);
+
+            /**
+             * @brief Check directory.
+             * @param[in] file File Directory
+             * @retval Returns true if file is directory, else false
+             */
+            static bool isDir(const std::string &file);
+
+            /**
+             * @brief Strip file extension.
+             * @param[in] path File Path
+             * @retval Returnes file strip extension
+             */
+            static std::string stripExtension(const std::string &path);
+
+            /**
+             * @brief Remove recursive files and directorys.
+             * @param[in] path File or directory
+             * @retval Returns true if all sucess, false otherwise
+             */
+            static bool remove(const std::string &path);
+    };
+}
+
+#endif /* __MSG_FILE_UTIL_H__ */
