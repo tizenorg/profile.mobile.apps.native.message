dcb982354eb8b808783fae1fb513ff0ddada3726
Denis Dolzhenko <d.dolzhenko@samsung.com>
2016-01-05 17:02:59 +0200

    Fixed:: build for tizen sdk 2.4
    Change-Id: If0b58b937ede9cf25b980e96e6892d0ed8e8962f
    Signed-off-by: Denis Dolzhenko <d.dolzhenko@samsung.com>

--- src/MainApp/MainApp.cpp
+++ src/MainApp/MainApp.cpp
@@ -116,6 +116,8 @@ void MainApp::initThemes()
 bool MainApp::onAppCreate()
 {
     TRACE;
+    MSG_LOG(LOCALEDIR);
+
     bindtextdomain(PROJECT_NAME, LOCALEDIR);
     elm_app_base_scale_set(1.8); // If the application is made based on HD(720x1280), set 2.4.
     elm_config_preferred_engine_set("opengl_x11");
