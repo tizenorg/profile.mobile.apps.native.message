4e1bb962fba1724dd0cd80d250c3551be9410e09
Gennadiy Azarov <gen.azarov@samsung.com>
2016-04-22 17:31:39 +0300

    Update of CLI build files
    Change-Id: I9c8dc3b0226286e2e104bd2911b406851a9d3d93
    Signed-off-by: Gennadiy Azarov <gen.azarov@samsung.com>

--- Build/flags.mk
+++ Build/flags.mk
@@ -7,7 +7,7 @@ CPP_OPTIMIZATION_OP = -O0
 
 COMPILE_FLAGS = $(DEBUG_OP) $(OPTIMIZATION_OP) 
 
-CPP_COMPILE_FLAGS = $(CPP_DEBUG_OP) $(CPP_OPTIMIZATION_OP) -std=c++0x -Wall -c -fmessage-length=0  -std=c++0x -fPIC 
+CPP_COMPILE_FLAGS = $(CPP_DEBUG_OP) $(CPP_OPTIMIZATION_OP) -std=c++0x -Wall -c -fmessage-length=0  -std=c++0x -Wno-extern-c-compat -fPIC 
 
 LINK_FLAGS = -shared -Wl,--no-undefined 
 
