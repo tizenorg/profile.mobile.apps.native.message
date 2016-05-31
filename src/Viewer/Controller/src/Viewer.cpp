08271a40be8baaed05d60f7e569c9b809e9e4065
Denis Dolzhenko <d.dolzhenko@samsung.com>
2016-05-31 16:23:49 +0300

    TizenRefApp-6398 Share attachment from slideshow
    Change-Id: Id8b51c1de2e81b2db5e6371302792b54da700b92
    Signed-off-by: Denis Dolzhenko <d.dolzhenko@samsung.com>

--- src/Viewer/Controller/src/Viewer.cpp
+++ src/Viewer/Controller/src/Viewer.cpp
@@ -27,6 +27,7 @@
 #include "ContactViewer.h"
 #include "FileUtils.h"
 #include "SaveAttachmentsPopup.h"
+#include "FileShare.h"
 
 #include <sstream>
 #include <iomanip>
@@ -269,6 +270,8 @@ void Viewer::onHwBackButtonClicked()
 
 void Viewer::onHwMoreButtonClicked()
 {
+    m_pSmilPlayer->stop();
+
     PopupList &popup = getApp().getPopupManager().getPopupList();
     popup.setAutoDismissBlockClickedFlag(true);
     popup.appendItem(msg("IDS_MSG_OPT_DELETE"), POPUPLIST_ITEM_PRESSED_CB(Viewer, onDeleteItemPressed), this);
@@ -278,8 +281,13 @@ void Viewer::onHwMoreButtonClicked()
 
     popup.appendItem(msg("IDS_MSGF_OPT_FORWARD"), POPUPLIST_ITEM_PRESSED_CB(Viewer, onForwardItemPressed), this);
 
-    if(!m_Msg->getAttachmentList().isEmpty() || m_Msg->getMediaCount() > 0)
+    bool hasAttachment = !m_Msg->getAttachmentList().isEmpty() || m_Msg->getMediaCount() > 0;
+    if(hasAttachment)
+    {
         popup.appendItem(msg("IDS_MSG_OPT_SAVE_ATTACHMENTS_ABB"), POPUPLIST_ITEM_PRESSED_CB(Viewer, onSaveAttachmentsItemPressed), this);
+        popup.appendItem(msg("IDS_COM_BUTTON_SHARE"), POPUPLIST_ITEM_PRESSED_CB(Viewer, onShareItemPressed), this);
+    }
+
     popup.show();
 }
 
@@ -410,6 +418,16 @@ void Viewer::onSaveAttachmentsItemPressed(PopupListItem &item)
     SaveAttachmentsPopup *popup = new SaveAttachmentsPopup(getApp(), *m_Msg.get());
     getApp().getPopupManager().reset(*popup);
     popup->show();
+}
+
+void Viewer::onShareItemPressed(PopupListItem &item)
+{
+    MSG_LOG("");
+    item.getParent().destroy();
+
+    SmilPage *page = m_pSmilPlayer->getCurrentPage();
+    if(page)
+        FileShare::launch(page->getAttachments());
 }
 
 void Viewer::onRecipItemClicked(Evas_Object *obj, void *eventInfo)
