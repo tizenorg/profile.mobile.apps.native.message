ff880bebb129a0eaa8226c2ea4755479910c7ddb
Andrey Klimenko <and.klimenko@samsung.com>
2016-06-15 18:32:04 +0300

    fixed CLI-build
    Change-Id: I56268c87a76fd24de71695f1c4853e528cd7d90d
    Signed-off-by: Andrey Klimenko <and.klimenko@samsung.com>

--- Build/basedef.mk
+++ Build/basedef.mk
@@ -3,64 +3,22 @@
 C_DEPS +=
 
 USES_EFL = yes
+USES_USR_INC = yes
 
 SYSROOT = $(SBI_SYSROOT)
 
-ifneq ($(strip $(SYSROOT)),)
+USR_INCS := $(addprefix -I $(SYSROOT),$(PLATFORM_INCS_EX))
+EFL_INCS = 
 
-ifeq ($(strip $(USES_EFL)),yes)
-_EFL_INCS = ecore-1 \
-            e_dbus-1 edje-1 eet-1 \
-            efreet-1 eina-1 "eina-1/eina" \
-            eio-1 embryo-1 \
-            evas-1 elementary-1 ethumb-1 \
-            efl-extension
-_EFL_INC_ROOT = $(SYSROOT)/usr/include
-EFL_INCS = $(addprefix -I $(_EFL_INC_ROOT)/,$(_EFL_INCS))
+ifeq ($(strip $(PLATFORM_LIB_PATHS)),)
+RS_LIB_PATHS := "$(SYSROOT)/usr/lib"
+else
+RS_LIB_PATHS := $(addprefix -L$(SYSROOT),$(PLATFORM_LIB_PATHS))
 endif
 
-FLATFORM_INCS = $(EFL_INCS) \
-  -I"$(SYSROOT)/usr/include/libxml2" \
-  -I"$(SDK_PATH)/library" \
-  -I"$(SYSROOT)/usr/include" \
-  -I"$(SYSROOT)/usr/include/appcore-agent" \
-  -I"$(SYSROOT)/usr/include/appfw" \
-  -I"$(SYSROOT)/usr/include/badge" \
-  -I"$(SYSROOT)/usr/include/base" \
-  -I"$(SYSROOT)/usr/include/cairo" \
-  -I"$(SYSROOT)/usr/include/calendar-service2" \
-  -I"$(SYSROOT)/usr/include/ckm" \
-  -I"$(SYSROOT)/usr/include/contacts-svc" \
-  -I"$(SYSROOT)/usr/include/content" \
-  -I"$(SYSROOT)/usr/include/context-manager" \
-  -I"$(SYSROOT)/usr/include/device" \
-  -I"$(SYSROOT)/usr/include/dlog" \
-  -I"$(SYSROOT)/usr/include/dbus-1.0" \
-  -I"$(SYSROOT)/usr/include/ewebkit2-0" \
-  -I"$(SYSROOT)/usr/include/fontconfig" \
-  -I"$(SYSROOT)/usr/include/freetype2" \
-  -I"$(SYSROOT)/usr/include/gio-unix-2.0" \
-  -I"$(SYSROOT)/usr/include/glib-2.0" \
-  -I"$(SYSROOT)/usr/include/json-glib-1.0" \
-  -I"$(SYSROOT)/usr/include/json-glib-1.0/json-glib" \
-  -I"$(SYSROOT)/usr/include/location" \
-  -I"$(SYSROOT)/usr/include/media" \
-  -I"$(SYSROOT)/usr/include/media-content" \
-  -I"$(SYSROOT)/usr/include/messaging" \
-  -I"$(SYSROOT)/usr/include/minizip" \
-  -I"$(SYSROOT)/usr/include/network" \
-  -I"$(SYSROOT)/usr/include/notification" \
-  -I"$(SYSROOT)/usr/include/sensor" \
-  -I"$(SYSROOT)/usr/include/shortcut" \
-  -I"$(SYSROOT)/usr/include/storage" \
-  -I"$(SYSROOT)/usr/include/system" \
-  -I"$(SYSROOT)/usr/include/telephony" \
-  -I"$(SYSROOT)/usr/include/ui" \
-  -I"$(SYSROOT)/usr/include/web" \
-  -I"$(SYSROOT)/usr/include/wifi-direct" \
-  -I"$(SYSROOT)/usr/lib/dbus-1.0/include" \
-  -I"$(SYSROOT)/usr/lib/glib-2.0/include" \
-  -I"$(SYSROOT)/usr/include/appcore-watch" \
-  -I"$(SYSROOT)/usr/include/widget_service"
+RS_LIBRARIES := $(addprefix -l,$(RS_LIBRARIES_EX))
 
-endif
+PLATFORM_INCS = $(USR_INCS) $(EFL_INCS) \
+     -I"$(SDK_PATH)/library"
+
+OS_NAME := $(shell $(UNAME))
--- Build/flags.mk
+++ Build/flags.mk
@@ -7,9 +7,9 @@ CPP_OPTIMIZATION_OP = -O0
 
 COMPILE_FLAGS = $(DEBUG_OP) $(OPTIMIZATION_OP) 
 
-CPP_COMPILE_FLAGS = $(CPP_DEBUG_OP) $(CPP_OPTIMIZATION_OP) -std=c++0x -Wall -c -fmessage-length=0  -std=c++0x -Wno-extern-c-compat 
+CPP_COMPILE_FLAGS = $(CPP_DEBUG_OP) $(CPP_OPTIMIZATION_OP) -std=c++0x -Wall -c -fmessage-length=0  -std=c++0x -Wno-extern-c-compat -fPIC 
 
-LINK_FLAGS = 
+LINK_FLAGS = -shared -Wl,--no-undefined 
 
 AR_FLAGS = 
 
