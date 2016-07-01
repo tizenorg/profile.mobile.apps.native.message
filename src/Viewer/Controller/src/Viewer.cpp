b0ecbf825e3c3b6932debf1754b53bbf1029cc5c
Oleksander Kostenko <o.kostenko@samsung.com>
2016-07-01 15:47:30 +0300

    TizenRefApp-6506 Wrong page counter in slideshow
    Change-Id: I9e45e761ac9fc0cec8450618e8e9a72b9ce545fb
    Signed-off-by: Oleksander Kostenko <o.kostenko@samsung.com>

--- src/Viewer/Controller/src/Viewer.cpp
+++ src/Viewer/Controller/src/Viewer.cpp
@@ -130,7 +130,11 @@ void Viewer::createSubjectLayout()
     if(!m_pSubjectLayout)
     {
         m_pSubjectLayout = new SubjectLayout(*m_pLayout);
-        m_pSubjectLayout->setNumberOfPages(m_Msg->getPageList().getLength());
+
+        int numberOfPages = m_Msg->getPageList().getLength();
+        numberOfPages += m_Msg->getAttachmentList().isEmpty() ? 0 : 1;
+
+        m_pSubjectLayout->setNumberOfPages(numberOfPages);
         m_pSubjectLayout->show();
         m_pLayout->setSubject(*m_pSubjectLayout);
         updateSubject();
